ac60b18fc6bc532c1ed0bd77a278469eab1ce103
Srinivasa Rao Thandra <srinivasa.thandra@nike.com>
2019-12-30 12:48:41 -0800

    New changes added

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,30 @@
-console.log('Hello welcome to GraphQL Basics');
+/*import { message } from './myModule';
+import addNumbers, { subtractNumbers } from './math';
+
+console.log('Hello addition is', addNumbers(5, 6));
+console.log('Hello subtraction is', subtractNumbers(5, 6));*/
+
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type Definations (Schema)
+const typeDefs = `
+type Query{
+   hello: String! 
+}
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello() {
+            return 'This is my First GraphQL Query';
+        }
+    }
+}
+
+
+const server = new GraphQLServer({ typeDefs: typeDefs, resolvers: resolvers });
+
+server.start(() => {
+    console.log('Server is Started');
+});
